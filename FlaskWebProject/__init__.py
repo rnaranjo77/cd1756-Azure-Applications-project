0dc679aa5278b88440bdbf77ba223b1698a006ee
rnaranjo77 <raul.naranjo@chevron.com>
2026-01-15 17:07:16 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,10 @@ from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__)
+
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
+
 app.config.from_object(Config)
 
 # ------------------------------------------
