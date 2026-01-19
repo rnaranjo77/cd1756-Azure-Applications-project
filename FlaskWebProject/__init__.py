2a995de336f28c5477a41d235cb1322f4d9874b7
rnaranjo77 <raul.naranjo@chevron.com>
2026-01-19 17:10:06 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,7 +2,6 @@
 """
 The flask application package.
 """
-import os
 import logging
 from logging.handlers import RotatingFileHandler
 from flask import Flask
@@ -14,36 +13,24 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-# Create session directory (required for flask-session)
-os.makedirs(app.config.get("SESSION_FILE_DIR", "/home/site/wwwroot/.flask_session"), exist_ok=True)
-
-# Enable HTTPS reconstruction behind Azure front door
-app.wsgi_app = ProxyFix(
-    app.wsgi_app,
-    x_for=1,
-    x_proto=1,
-    x_host=1,
-    x_port=1,
-    x_prefix=1
-)
+# TODO: Add any logging levels and handlers with app.logger
 
 # Logging setup
 app.logger.setLevel(logging.INFO)
-file_handler = RotatingFileHandler("app.log", maxBytes=1_000_000, backupCount=3)
-file_formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s")
-file_handler.setFormatter(file_formatter)
-app.logger.addHandler(file_handler)
+handler = RotatingFileHandler("app.log", maxBytes=1_000_000, backupCount=3)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s")
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
 
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-stream_handler.setFormatter(file_formatter)
-app.logger.addHandler(stream_handler)
+# Trust proxy headers for HTTPS
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_view = 'login'  
 
 import FlaskWebProject.views
-
+from FlaskWebProject import views 
